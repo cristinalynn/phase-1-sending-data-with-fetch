fe52e98e46fe76e2580209c7a91fd54bcdf926b1
Cristina Lynn <cristina@cristinalynn.net>
2023-01-31 14:16:28 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    return  fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            document.body.innerHTML = object[ "id" ]
+            // const element = document.createElement('div')
+            // element.innerHTML = object.id
+            // document.appendChild(element)
+            console.log(object, 'my object');
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+
+}
